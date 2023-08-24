1a9dbbd2566121ca6e77275d5395f5861a881965
Sawyer Vaughan <sawyervaughan@gmail.com>
2023-08-24 18:25:04 +0100

    Document AlertManager class

--- OpenFoods/UI/AlertManager.swift
+++ OpenFoods/UI/AlertManager.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
+/// The object that manages the presentation of alerts to the user.
+///
+/// This class simply holds the state of the presented alert, and RootView wires this together with
+/// the UI, so that an alert is presented when `alertText` is set.
 class AlertManager: ObservableObject {
+  /// Whether an alert should be presented.
   var isPresented: Bool {
     get {
       alertText != nil
@@ -20,6 +25,7 @@ class AlertManager: ObservableObject {
   
   @Published private(set) var alertText: String? = nil
   
+  /// Presents the user with a dismissable alert showing the given text.
   func presentAlert(text: String) {
     alertText = text
   }
