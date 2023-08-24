3d441763964860cb9661ba855e844ecce976636e
Sawyer Vaughan <sawyervaughan@gmail.com>
2023-08-24 14:57:49 +0100

    Re-create the Xcode project with SwiftUI base

---
+++ OpenFoods/OpenFoods/OpenFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenFoodsApp.swift
+//  OpenFoods
+//
+//  Created by Sawyer Vaughan on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
